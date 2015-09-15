8bd676a95cf9bc18d020b31b08787d8ceae55395
Bobby <bellfalasch@gmail.com>
2015-09-15 13:07:06 +0200

    More contentLib upgrades

--- src/main/resources/site/parts/images1b/images1b.js
+++ src/main/resources/site/parts/images1b/images1b.js
@@ -1,6 +1,6 @@
 var portal = require('/lib/xp/portal'); // Import the portal functions
 var thymeleaf = require('/lib/xp/thymeleaf'); // Import the Thymeleaf rendering function
-var lib = require('/lib/xp/content');
+//var contentLib = require('/lib/xp/content');
 var util = require('/lib/utilities'); // My own extension functions
 
 // Handle the GET request
@@ -14,7 +14,7 @@ exports.get = function(req) {
 //	util.log(component);
 
 	var content = component.config;
-		
+
 	var imageUrl = null;
 
 	// Get the true image URL (only the ID to it is stored in Enonic)
